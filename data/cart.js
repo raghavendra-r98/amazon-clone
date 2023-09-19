bc464a7ef667838eace043ea6e2aeb019be9cdd5
raghavendra-r98 <145101236+raghavendra-r98@users.noreply.github.com>
2023-09-19 18:58:34 +0530

    Add files via upload

---
+++ data/cart.js
@@ -0,0 +1,54 @@
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if(!cart){
+  cart = [{
+    productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2,
+  },{
+    productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1
+  }];
+}
+
+
+function saveToStorage() {
+  localStorage.setItem('cart',JSON.stringify(cart));
+}
+
+//this is add to cart function
+export function addToCart(productId)
+{
+  let matchingItem ;
+
+    cart.forEach((cartItem)=>{
+      if(productId === cartItem.productId ){
+        matchingItem = cartItem;
+      }
+    });
+      //if same product
+    if(matchingItem){
+      matchingItem.quantity+=1;
+    } else {
+      cart.push({  //new item
+        productId: productId,
+        quantity: 1
+      });
+    }
+
+    saveToStorage();
+} 
+
+export function removeFromCart(productId) {
+  const newCart = [];
+
+  cart.forEach((cartItem) => {
+    if(cartItem.productId !== productId)
+    {
+      newCart.push(cartItem);
+    } 
+  });
+
+  cart = newCart;
+
+  saveToStorage();
+}
